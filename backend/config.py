36f8515d4105ad25eb60910c2fa467f9295b889e
Aarthi-2006 <006aarthi@gmail.com>
2026-09-12 18:54:25 +0530

    Prepare backend for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,38 @@
+import os
+import tempfile
+
+# Aiven SSL certificate
+ssl_ca_content = os.environ.get("AIVEN_SSL_CA")
+
+ssl_ca_path = None
+
+if ssl_ca_content:
+    ssl_ca_path = os.path.join(tempfile.gettempdir(), "aiven-ca.pem")
+
+    with open(ssl_ca_path, "w") as f:
+        f.write(ssl_ca_content)
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "11223344",
-    "database": "bondbridge_ai"
+    "host": os.environ.get(
+        "DB_HOST",
+        "mysql-1451f425-tcarts-248.g.aivencloud.com"
+    ),
+
+    "port": int(os.environ.get("DB_PORT", "28142")),
+
+    "user": os.environ.get(
+        "DB_USER",
+        "avnadmin"
+    ),
+
+    "password": os.environ.get(
+        "DB_PASSWORD"
+    ),
+
+    "database": os.environ.get(
+        "DB_NAME",
+        "bondbridge_ai"
+    ),
+
+    "ssl_ca": ssl_ca_path
 }
--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,16 @@
 import mysql.connector
+
 from config import DB_CONFIG
 
 def get_connection():
+
     return mysql.connector.connect(
+
         host=DB_CONFIG["host"],
+        port=DB_CONFIG["port"],
         user=DB_CONFIG["user"],
         password=DB_CONFIG["password"],
-        database=DB_CONFIG["database"]
+        database=DB_CONFIG["database"],
+        ssl_ca=DB_CONFIG["ssl_ca"]
+
     )
